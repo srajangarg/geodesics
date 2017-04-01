8a000f0e3bda5b45a26c0026c9b02cd488179ddd
Srajan Garg <srajan.garg@gmail.com>
2017-04-01 20:14:15 +0530

    changes

--- Algo.h
+++ Algo.h
@@ -49,7 +49,7 @@ public:
         return 0.0;
     }
 
-    bool operator<(const Interval &rhs)
+    bool operator<(const Interval &rhs) const
     {
         if (min_d != rhs.min_d)
             return min_d < rhs.min_d;
@@ -115,6 +115,13 @@ public:
         // with the closest source indicated on each interval
     }
 
+    vector<Interval> sanitize_and_merge(vector<Interval>& intervals)
+    {
+        // FILL
+
+        return {};
+    }
+
     void insert_new_interval(Edge *e, Interval &new_w)
     {
         // FILL
@@ -122,7 +129,7 @@ public:
         // should be O(edge_intervals[e])
 
         auto & intervals = edge_intervals[e];
-        list<Interval> new_intervals;
+        vector<Interval> new_intervals;
         bool new_pushed = false;
 
         auto it = intervals.begin();
@@ -177,11 +184,24 @@ public:
             {
                 // --------new----------
                 //      -----w-----
+
+                Interval new_w_short(new_w); new_w_short.end = w->st;
+                new_intervals.push_back(new_w_short);
+                for (auto &interval : source_bisect(w->st, w->end, new_w.pos, w->pos))
+                    new_intervals.push_back(interval);
+                new_w.st = w->end;
             }
             else if (w->st < new_w.st and w->end > new_w.end)
             {
                 //     ----new----
                 //  ----------w-------
+
+                Interval w_short1(*w); w_short1.end = new_w.st;
+                new_intervals.push_back(w_short1);
+                for (auto &interval : source_bisect(new_w.st, new_w.end, new_w.pos, w->pos))
+                    new_intervals.push_back(interval);
+                Interval w_short2(*w); w_short2.st = new_w.end;
+                new_intervals.push_back(w_short2);
                 new_pushed = true;
             }
             else
@@ -198,10 +218,20 @@ public:
         }
 
         for (; it != intervals.end(); it++)
-        {
             new_intervals.push_back(**it);
-        }
 
+        for (auto &it : intervals)
+            intervals_heap.erase(it);
+        intervals.clear();
+
+        auto sanitized_new_intervals = sanitize_and_merge(new_intervals);
+
+        for (auto interval : sanitized_new_intervals)
+        {
+            auto pp = intervals_heap.insert(interval);
+            assert(pp.second);
+            intervals.push_back(pp.first);
+        }
     }
 
     vector<Interval> get_new_intervals(const Interval &w, Edge *e)
