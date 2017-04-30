93b7252dda44c0ef94e262c61ba3b1318c508737
Srajan Garg <srajan.garg@gmail.com>
2017-04-30 21:33:04 +0530

    new untis

---
+++ NewIntv.cpp
@@ -0,0 +1,245 @@
+#include "Algo.h"
+
+vector<Interval> MMP::source_bisect(double st, double end, const Interval &i1,
+                               const Interval &i2)
+{
+    // takes in  a segment st-end and returns 1or2 interval
+    // with the closest source indicated on each interval
+    auto ps1 = i1.pos, ps2 = i2.pos;
+    Vector2 st_v(st, 0), end_v(end, 0);
+    vector<Interval> b_intervals;
+    double x, alpha, beta, gamma;
+
+    alpha = ps2.x() - ps1.x();
+    beta = i2.ps_d - i1.ps_d;
+    gamma = ps1.squaredLength() - ps2.squaredLength() - beta * beta;
+
+    // ax^2 + bx + c = 0
+    double a = alpha * alpha - beta * beta;
+    double b = gamma * alpha + 2.0 * ps2.x() * beta * beta;
+    double c = 0.25 * gamma * gamma - ps2.squaredLength() * beta * beta;
+    bool equidist_pt = true;
+
+    if ((b * b - 4 * a * c < 0) or (abs(b) < EPS and abs(c) < EPS))
+        equidist_pt = false;
+    else if (abs(a) < EPS)
+        x = -c / b;
+    else
+    {
+        x = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
+        if (x <= st or x >= end)
+            x = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
+    }
+
+    // cout<<"a "<<a<<" b "<<b<<" c "<<c<<endl;
+    if (equidist_pt and x > st and x < end) {
+        // [st, x] closer to ps1 or ps2?
+        auto i1val = ((ps1 - st_v).length() + i1.ps_d);
+        auto i2val = ((ps2 - st_v).length() + i2.ps_d);
+
+        if (i1val < i2val) {
+            b_intervals.push_back(Interval(ps1, st, x, i1));
+            b_intervals.push_back(Interval(ps2, x, end, i2));
+        } else {
+            b_intervals.push_back(Interval(ps2, st, x, i2));
+            b_intervals.push_back(Interval(ps1, x, end, i1));
+        }
+    } else {
+        // the complete interval is near to one single source
+        auto i1val = ((ps1 - st_v).length() + i1.ps_d);
+        auto i2val = ((ps2 - st_v).length() + i2.ps_d);
+
+        if (abs(i1val -i2val) < EPS)
+        {
+            i1val = ((ps1 - end_v).length() + i1.ps_d);
+            i2val = ((ps2 - end_v).length() + i2.ps_d);
+        }
+
+        if (i2val < i1val)
+            b_intervals.push_back(Interval(ps2, st, end, i2));
+        else
+            b_intervals.push_back(Interval(ps1, st, end, i1));
+    }
+
+    return b_intervals;
+}
+
+vector<Interval> MMP::sanitize_and_merge(vector<Interval> &intervals)
+{
+    // remove too small intervals, merge intervals with same pos and ps_d etc.
+    vector<Interval> sanitized_intervals;
+    Interval last_merged_interval = intervals[0];
+    auto it1 = intervals.begin();
+    auto it2 = it1;
+    // last_merged_interval is interval corresponding to [it1, it2)
+    for (it2++; it1 != intervals.end() and it2 != intervals.end();) {
+
+        auto w = *it2;
+        // check if w is too small
+        if (w.end - w.st > EPS) {
+            // check if last merged interval and w have same pos and everything
+            if ((last_merged_interval.pos - w.pos).length() < EPS
+                and abs(last_merged_interval.ps_d - w.ps_d) < EPS) {
+                // merge last_merge_interval and w
+                last_merged_interval.end = w.end;
+                last_merged_interval.recompute_min_d();
+                it2++;
+            } else {
+
+                if (abs(last_merged_interval.end - last_merged_interval.st) > EPS)
+                    sanitized_intervals.push_back(last_merged_interval);
+
+                it1 = it2;
+                it2++;
+                last_merged_interval = *it1;
+            }
+        } else
+            it2++;
+    }
+    sanitized_intervals.push_back(last_merged_interval);
+    return sanitized_intervals;
+}
+
+void MMP::insert_new_interval(Interval &new_w)
+{
+    // updates edge_intervals[e] and intervals according to algo discussed
+    // should be O(edge_intervals[e])
+    cout << "Inserting new interval " << new_w << endl;
+
+    auto &intervals = edge_intervals[new_w.edge];
+    vector<Interval> new_intervals;
+    bool new_fully_pushed = false;
+
+    auto it = intervals.begin();
+    for (; it != intervals.end(); it++) {
+        // COPY happening here!!!
+        auto w = *it;
+
+        if (new_fully_pushed)
+            break;
+
+        if (w.st >= new_w.end) {
+            // ----new----
+            //               -----w-----
+            new_intervals.push_back(new_w);
+            new_intervals.push_back(w);
+            new_fully_pushed = true;
+        } else if (w.end <= new_w.st) {
+            //             -----new-----
+            // ----w----
+            new_intervals.push_back(w);
+        } else if (w.st < new_w.st and w.end <= new_w.end) {
+            //       -------new------
+            // ------w--------
+
+            Interval w_short(w);
+            w_short.end = new_w.st;
+
+            new_intervals.push_back(w_short);
+            for (auto &interval : source_bisect(new_w.st, w.end, w, new_w))
+                new_intervals.push_back(interval);
+
+            new_w.st = w.end;
+        } else if (w.st >= new_w.st and w.end > new_w.end) {
+            // ------new------
+            //        -----w--------
+
+            Interval new_w_short(new_w);
+            new_w_short.end = w.st;
+            new_intervals.push_back(new_w_short);
+            for (auto &interval : source_bisect(w.st, new_w.end, new_w, w))
+                new_intervals.push_back(interval);
+            Interval w_short(w);
+            w_short.st = new_w.end;
+            new_intervals.push_back(w_short);
+
+            new_fully_pushed = true;
+        } else if (w.st >= new_w.st and w.end <= new_w.end) {
+            // --------new----------
+            //      -----w-----
+
+            Interval new_w_short(new_w);
+            new_w_short.end = w.st;
+            new_intervals.push_back(new_w_short);
+
+            // cout<<"source bisect- ----"<<endl;
+            // cout<<w.st<<" "<<w.end<<endl;
+            // cout<<"i1 : "<<new_w<<endl;
+            // cout<<"i2 : "<<(w)<<endl;
+            for (auto &interval : source_bisect(w.st, w.end, new_w, w))
+                new_intervals.push_back(interval);
+            // cout<<"----"<<endl;
+
+
+
+            new_w.st = w.end;
+        } else if (w.st < new_w.st and w.end > new_w.end) {
+            //     ----new----
+            //  ----------w-------
+
+            Interval w_short1(w);
+            w_short1.end = new_w.st;
+            new_intervals.push_back(w_short1);
+            for (auto &interval : source_bisect(new_w.st, new_w.end, new_w, w))
+                new_intervals.push_back(interval);
+            Interval w_short2(w);
+            w_short2.st = new_w.end;
+            new_intervals.push_back(w_short2);
+            new_fully_pushed = true;
+        } else {
+            // should never be reached
+            assert(false);
+        }
+    }
+
+    if (not new_fully_pushed) {
+        assert(it == intervals.end());
+        new_intervals.push_back(new_w);
+    }
+
+    for (; it != intervals.end(); it++)
+        new_intervals.push_back(*it);
+
+    for (auto it = intervals.begin(); it != intervals.end(); it++)
+        intervals_heap.erase(it);
+
+    // set<Interval *> not_to_delete;
+    // COPY happening here
+    auto old_intervals = intervals;
+    intervals.clear();
+
+    auto sanitized_new_intervals = sanitize_and_merge(new_intervals);
+    cout << "Old intervals cleared, all intervals on " << *new_w.edge
+         << " are :" << endl;
+
+    for (auto &interval : sanitized_new_intervals) {
+
+        Interval* keep_old_itv = NULL;
+        for (auto &old_itv : old_intervals)
+            if (interval == old_itv)
+                keep_old_itv = &old_itv;
+
+        interval.recompute_min_d();
+        cout << interval << endl;
+
+        if (keep_old_itv) {
+            intervals.push_back(*keep_old_itv);
+            // not_to_delete.insert(keep_old_itv);
+            if (not keep_old_itv->propagated)
+            {
+                auto pp = intervals_heap.insert(--intervals.end());
+                assert(pp.second);
+            }
+        } else {
+            // auto added_intv = new Interval(interval);
+            intervals.push_back(interval);
+            auto pp = intervals_heap.insert(--intervals.end());
+            assert(pp.second);
+        }
+    }
+    cout << endl;
+
+    // for (auto &itv : old_intervals)
+    //     if (not_to_delete.find(itv) == not_to_delete.end())
+    //         delete itv;
+}
---
+++ Propagation.cpp
@@ -0,0 +1,108 @@
+#include "Algo.h"
+
+vector<Interval> MMP::prop_thru_interval(Vector2 src, Interval &w, Face *face, double ps_d)
+{
+    // face is the face on which the intervals propogate
+    // check whether src lies on the interval
+    vector<Interval> new_intervals;
+    if (src.y() < EPS and (src.x() < w.st or src.x() > w.end))
+        return {};
+
+    for (auto &edge : face->edges) {
+        if (edge == w.edge)
+            continue;
+
+        auto info = w.get_info_edge(src, edge, face);
+        bool invert = info.common > edge->getOtherEndpoint(info.common);
+
+        if (info.possible and info.e1 < edge->length()) {
+
+            double x = info.r * cos(info.angle);
+            if (abs(x) < EPS)
+                x = 0;
+
+            double y = abs(info.r * sin(info.angle));
+            if (y < EPS)
+                y = 0;
+
+            new_intervals.push_back(Interval(x, y, info.e1,
+                                             min(info.e2, edge->length()), ps_d, face,
+                                             edge, invert));
+        }
+        // otherwise doesn't intersect this edge
+    }
+
+    return new_intervals;
+}
+
+void MMP::propagate()
+{
+	auto top_ite = *intervals_heap.begin();
+    Interval prop_w = *top_ite;
+    Edge *prop_e = prop_w.edge;
+
+	intervals_heap.erase(intervals_heap.begin());
+    update_not_reached(top_ite);
+
+    // should this be done before or after inserting new intervals?
+    top_ite->propagated = true;
+
+    for (auto &face : prop_e->faces) {
+        if (prop_w.from == face)
+            continue;
+
+        cout << "Propagating " << prop_w << " on " << *face << endl;
+        auto candidates = get_new_intervals(prop_w, face);
+
+        cout << endl;
+        for (auto &new_w : candidates)
+            insert_new_interval(new_w);
+    }
+}
+
+vector<Interval> MMP::get_new_intervals(Interval &w, Face *face)
+{
+    // propogate this interval to edges on `face`
+    Edge *edge = w.edge;
+    vector<Interval> new_intervals;
+    Vertex *v0 = edge->getEndpoint(0), *v1 = edge->getEndpoint(1);
+
+    if (w.st < EPS and v0->saddle_or_boundary) {
+        // treat v0 as a new source
+
+        Vector2 new_src = Vector2(0, 0);
+        auto intervals = prop_thru_interval(new_src, w, face,
+                                            w.ps_d + (new_src - w.pos).length());
+
+        if (intervals.size())
+            cout << intervals.size() << " to be added for saddle " << *v0 << ":"
+                 << endl;
+
+        for (auto &ii : intervals)
+            cout << ii << endl, new_intervals.push_back(ii);
+    }
+
+    if (edge->length() - w.end < EPS and v1->saddle_or_boundary) {
+        // treat v1 as a new source
+
+        Vector2 new_src = Vector2(edge->length(), 0);
+
+        auto intervals = prop_thru_interval(new_src, w, face,
+                                            w.ps_d + (new_src - w.pos).length());
+        if (intervals.size())
+            cout << intervals.size() << " to be added for saddle " << *v1 << ":"
+                 << endl;
+
+        for (auto &ii : intervals)
+            cout << ii << endl, new_intervals.push_back(ii);
+    }
+
+    // generate intervals from (usual) current source
+    auto intervals = prop_thru_interval(w.pos, w, face, w.ps_d);
+
+    if (intervals.size())
+        cout << intervals.size() << " to be added for usual propogate:" << endl;
+    for (auto &ii : intervals)
+        cout << ii << endl, new_intervals.push_back(ii);
+    return new_intervals;
+}
