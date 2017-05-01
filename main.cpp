5ef78e2712594b377d6b61916425a788add286ff
ahemanthkumar <angajalahemanthkumar510@gmail.com>
2017-05-01 10:50:44 +0530

    final pres

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "Algo.h"
 #include "Djk.h"
 #include <cstdlib>
+#include <chrono>
 
 int usage(int argc, char *argv[])
 {
@@ -33,11 +34,34 @@ int main(int argc, char *argv[])
 
     MMP mmp(&mesh, p, d);
     DJK djk(&mesh, p, d);
-    // cout << "src : vertex " << mesh.vertices.front().index << endl;
-    // cout << "dest : vertex " << mesh.vertices.back().index << endl;
-    // auto path1 = mmp.algorithm();
+
+    double len1 = 0, len2 = 0;
+
+    auto t1 = std::chrono::high_resolution_clock::now();
     auto path1 = mmp.algorithm();
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto tt1 = chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+
+    for (uint i = 1; i < path1.size(); i++)
+        len1 += (path1[i-1].pos - path1[i].pos).length();
+
+    t1 = std::chrono::high_resolution_clock::now();
     auto path2 = djk.algorithm();
+    t2 = std::chrono::high_resolution_clock::now();
+    auto tt2 = chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+
+    for (uint i = 1; i < path2.size(); i++)
+        len2 += (path2[i-1].pos - path2[i].pos).length();
+
+    cout<<endl;
+    cout<<"Path length MMP : "<<len1<<endl;
+    cout<<"Path length DJK : "<<len2<<endl;
+    cout<<"Ratio : "<<(len1/len2)<<endl<<endl;
+
+    cout<<endl;
+    cout<<"Time taken MMP : "<<tt1<<" us"<<endl;
+    cout<<"Time taken DJK : "<<tt2<<" us"<<endl;
+    cout<<"Ratio : "<<(tt1/tt2)<<endl<<endl;
 
     Viewer viewer;
     viewer.setObject(&mesh, path1, path2);
